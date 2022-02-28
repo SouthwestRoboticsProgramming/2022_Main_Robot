6bb5d15521b787887d4c81315a5a840c50576fce
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-02-28 16:25:08 -0600

    Add Climb Command

--- SwerveRobot/src/main/java/frc/robot/command/climb/ClimbCommand.java
+++ SwerveRobot/src/main/java/frc/robot/command/climb/ClimbCommand.java
@@ -1,5 +1,19 @@
 package frc.robot.command.climb;
 
-public class ClimbCommand {
-    
+import frc.robot.Robot;
+import frc.robot.command.CommandSequence;
+
+public class ClimbCommand extends CommandSequence {
+    public ClimbCommand() {
+        // Add the climb steps here
+    }
+
+    @Override
+    public boolean run() {
+        if (Robot.INSTANCE.input.getClimberNextStep()) {
+            next();
+        }
+
+        return super.run();
+    }
 }
