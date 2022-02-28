7a4f81caaeb2e47f851f5f1e8a08988638c1cbcc
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-02-28 12:23:35 -0600

    test commit

--- SwerveRobot/src/main/java/frc/robot/control/XboxController.java
+++ SwerveRobot/src/main/java/frc/robot/control/XboxController.java
@@ -1,116 +1,51 @@
 package frc.robot.control;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
 public final class XboxController {
-    private final Joystick joystick;
-    private final JoystickButton a;
-    private final JoystickButton b;
-    private final JoystickButton x;
-    private final JoystickButton y;
-    private final JoystickButton menu;
-    private final JoystickButton window;
-    private final JoystickButton leftShoulder;
-    private final JoystickButton rightShoulder;
-    private final JoystickButton leftStick;
-    private final JoystickButton rightStick;
+    public interface Button {
+        boolean isPressed();
+        boolean leadingEdge();
+        boolean fallingEdge();
 
-    public XboxController(int id) {
-        joystick = new Joystick(id);
-        a = new JoystickButton(joystick, 1);
-        b = new JoystickButton(joystick, 2);
-        x = new JoystickButton(joystick, 3);
-        y = new JoystickButton(joystick, 4);
-        leftShoulder = new JoystickButton(joystick, 5);
-        rightShoulder = new JoystickButton(joystick, 6);
-        window = new JoystickButton(joystick, 7);
-        menu = new JoystickButton(joystick, 8);
-        leftStick = new JoystickButton(joystick, 9);
-        rightStick = new JoystickButton(joystick, 10);
+        void update();
     }
 
-    public double getLeftStickX() {
-        return joystick.getRawAxis(0);
+    public interface Axis {
+        public double get();
+
+        void update();
     }
 
-    public double getLeftStickY() {
-        return -joystick.getRawAxis(1);
-    }
+    public Button a, b, x, y;
+    public Button leftShoulder, rightShoulder;
+    public Button select, start;
+    public Button dpadUp, dpadDown, dpadLeft, dpadRight;
+    public Button leftStickIn, rightStickIn;
 
-    public double getRightStickX() {
-        return joystick.getRawAxis(4  );
-    }
+    public Axis leftStickX, leftStickY;
+    public Axis rightStickX, rightStickY;
+    public Axis leftTrigger, rightTrigger;
 
-    public double getRightStickY() {
-        return -joystick.getRawAxis(5);
-    }
+    public void update() {
+        a.update();
+        b.update();
+        x.update();
+        y.update();
+        leftShoulder.update();
+        rightShoulder.update();
+        select.update();
+        start.update();
+        dpadUp.update();
+        dpadDown.update();
+        dpadLeft.update();
+        dpadRight.update();
+        leftStickIn.update();
+        rightStickIn.update();
 
-    public double getLeftTrigger() {
-        return joystick.getRawAxis(2);
-    }
-
-    public double getRightTrigger() {
-        return joystick.getRawAxis(3);
-    }
-
-    public boolean getLeftStickButton() {
-        return leftStick.get();
-    }
-
-    public boolean getRightStickButton() {
-        return rightStick.get();
-    }
-
-    public boolean getAButton() {
-        return a.get();
-    }
-
-    public boolean getBButton() {
-        return b.get();
-    }
-
-    public boolean getXButton() {
-        return x.get();
-    }
-
-    public boolean getYButton() {
-        return y.get();
-    }
-
-    public boolean getDpadUp() {
-        int pov = joystick.getPOV();
-        return pov == 0 || pov == 45 || pov == 315;
-    }
-
-    public boolean getDpadDown() {
-        int pov = joystick.getPOV();
-        return pov == 180 || pov == 135 || pov == 225;
-    }
-
-    public boolean getDpadLeft() {
-        int pov = joystick.getPOV();
-        return pov == 270 || pov == 225 || pov == 315;
-    }
-
-    public boolean getDpadRight() {
-        int pov = joystick.getPOV();
-        return pov == 90 || pov == 45 || pov == 135;
-    }
-
-    public boolean getLeftShoulderButton() {
-        return leftShoulder.get();
-    }
-
-    public boolean getRightShoulderButton() {
-        return rightShoulder.get();
-    }
-
-    public boolean getMenuButton() {
-        return menu.get();
-    }
-
-    public boolean getWindowButton() {
-        return window.get();
+        leftStickX.update();
+        leftStickY.update();
+        rightStickX.update();
+        rightStickY.update();
+        leftTrigger.update();
+        rightTrigger.update();
     }
 }
